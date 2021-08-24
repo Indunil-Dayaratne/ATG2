6660a4b13e38cd49ad55e598a523e7ae539b4d76
Indunil Dayaratne <Indunil.Dayaratne@britinsurance.com>
2021-08-24 23:03:45 +0100

    Implemented all the lot service tests

--- ATG.CodeTest.Tests/LotServiceTests.cs
+++ ATG.CodeTest.Tests/LotServiceTests.cs
@@ -62,25 +62,118 @@ namespace ATG.CodeTest.Tests
         [Fact]
         public async Task GetLotAsyncLoadsDataFromFailoverDataStore()
         {
+            const int id = 123;
 
+            var mockFailoverLotRepository = new Mock<IFailoverLotRepository>();
+            var mockArchivedRepository = new Mock<IArchivedRepository>();
+            var mockLotRepository = new Mock<ILotRepository>();
+            var mockFailoverLotEntryDataLoader = new Mock<IFailoverLotEntryDataLoader>();
+            var mockCurrentDateTimeProvider = new Mock<ICurrentDateTimeProvider>();
+
+            var service = new LotService(true, 2, mockFailoverLotRepository.Object, mockArchivedRepository.Object,
+                mockLotRepository.Object, mockFailoverLotEntryDataLoader.Object, mockCurrentDateTimeProvider.Object);
+
+            mockCurrentDateTimeProvider.Setup(x => x.GetCurrentDateTime()).Returns(DateTime.Now.AddMinutes(-30));
+            mockFailoverLotEntryDataLoader.Setup(x => x.GetFailOverLotEntriesAsync())
+                .ReturnsAsync(new List<FailoverLots>
+                {
+                    new FailoverLots {DateTime = DateTime.Now}, 
+                    new FailoverLots {DateTime = DateTime.Now},
+                    new FailoverLots {DateTime = DateTime.Now}
+                });
+
+            mockFailoverLotRepository.Setup(x => x.GetLotAsync(id)).ReturnsAsync(new Lot {Id = id});
+
+            await service.GetLotAsync(id, false);
+
+            mockArchivedRepository.Verify(x => x.GetLotAsync(id), Times.Never);
+            mockFailoverLotRepository.Verify(x => x.GetLotAsync(id), Times.Once);
+            mockLotRepository.Verify(x => x.GetLotAsync(id), Times.Never);
         }
 
         [Fact]
         public async Task GetLotAsyncLoadsDataFromBothFailoverAndArchivedDataStores()
         {
+            const int id = 123;
 
+            var mockFailoverLotRepository = new Mock<IFailoverLotRepository>();
+            var mockArchivedRepository = new Mock<IArchivedRepository>();
+            var mockLotRepository = new Mock<ILotRepository>();
+            var mockFailoverLotEntryDataLoader = new Mock<IFailoverLotEntryDataLoader>();
+            var mockCurrentDateTimeProvider = new Mock<ICurrentDateTimeProvider>();
+
+            var service = new LotService(true, 2, mockFailoverLotRepository.Object, mockArchivedRepository.Object,
+                mockLotRepository.Object, mockFailoverLotEntryDataLoader.Object, mockCurrentDateTimeProvider.Object);
+
+            mockCurrentDateTimeProvider.Setup(x => x.GetCurrentDateTime()).Returns(DateTime.Now.AddMinutes(-30));
+            mockFailoverLotEntryDataLoader.Setup(x => x.GetFailOverLotEntriesAsync())
+                .ReturnsAsync(new List<FailoverLots>
+                {
+                    new FailoverLots {DateTime = DateTime.Now},
+                    new FailoverLots {DateTime = DateTime.Now},
+                    new FailoverLots {DateTime = DateTime.Now}
+                });
+
+            mockFailoverLotRepository.Setup(x => x.GetLotAsync(id)).ReturnsAsync(new Lot { Id = id, IsArchived = true});
+
+            await service.GetLotAsync(id, false);
+
+            mockArchivedRepository.Verify(x => x.GetLotAsync(id), Times.Once);
+            mockFailoverLotRepository.Verify(x => x.GetLotAsync(id), Times.Once);
+            mockLotRepository.Verify(x => x.GetLotAsync(id), Times.Never);
         }
 
         [Fact]
         public async Task GetLotAsyncLoadsDataFromMainDataStore()
         {
+            var mockFailoverLotRepository = new Mock<IFailoverLotRepository>();
+            var mockArchivedRepository = new Mock<IArchivedRepository>();
+            var mockLotRepository = new Mock<ILotRepository>();
+            var mockFailoverLotEntryDataLoader = new Mock<IFailoverLotEntryDataLoader>();
+            var mockCurrentDateTimeProvider = new Mock<ICurrentDateTimeProvider>();
 
+            var service = new LotService(true, 50, mockFailoverLotRepository.Object, mockArchivedRepository.Object,
+                mockLotRepository.Object, mockFailoverLotEntryDataLoader.Object, mockCurrentDateTimeProvider.Object);
+
+            mockFailoverLotEntryDataLoader.Setup(x => x.GetFailOverLotEntriesAsync())
+                .ReturnsAsync(new List<FailoverLots>());
+
+            mockCurrentDateTimeProvider.Setup(x => x.GetCurrentDateTime()).Returns(DateTime.UtcNow);
+
+            const int id = 123;
+
+            await service.GetLotAsync(id, false);
+
+            mockArchivedRepository.Verify(x => x.GetLotAsync(id), Times.Never);
+            mockFailoverLotRepository.Verify(x => x.GetLotAsync(id), Times.Never);
+            mockLotRepository.Verify(x => x.GetLotAsync(id), Times.Once);
         }
 
         [Fact]
         public async Task GetLotAsyncLoadsDataFromBothMainAndArchivedDataStores()
         {
+            const int id = 123;
 
+            var mockFailoverLotRepository = new Mock<IFailoverLotRepository>();
+            var mockArchivedRepository = new Mock<IArchivedRepository>();
+            var mockLotRepository = new Mock<ILotRepository>();
+            var mockFailoverLotEntryDataLoader = new Mock<IFailoverLotEntryDataLoader>();
+            var mockCurrentDateTimeProvider = new Mock<ICurrentDateTimeProvider>();
+
+            var service = new LotService(true, 50, mockFailoverLotRepository.Object, mockArchivedRepository.Object,
+                mockLotRepository.Object, mockFailoverLotEntryDataLoader.Object, mockCurrentDateTimeProvider.Object);
+
+            mockFailoverLotEntryDataLoader.Setup(x => x.GetFailOverLotEntriesAsync())
+                .ReturnsAsync(new List<FailoverLots>());
+
+            mockCurrentDateTimeProvider.Setup(x => x.GetCurrentDateTime()).Returns(DateTime.UtcNow);
+            mockLotRepository.Setup(x => x.GetLotAsync(id)).ReturnsAsync(new Lot { Id = id, IsArchived = true });
+
+            await service.GetLotAsync(id, false);
+
+            mockArchivedRepository.Verify(x => x.GetLotAsync(id), Times.Once);
+            mockFailoverLotRepository.Verify(x => x.GetLotAsync(id), Times.Never);
+            mockLotRepository.Verify(x => x.GetLotAsync(id), Times.Once);
         }
     }
 }
